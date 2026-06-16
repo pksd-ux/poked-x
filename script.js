2e0f67748bde6673d30eb8f1b81918936ee17b40
Seu Nome <seu-email@example.com>
2026-06-15 22:35:12 -0300

    js finalizado

--- script.js
+++ script.js
@@ -16,8 +16,92 @@ formulario.addEventListener('submit', async function (evento ) {
     let dados = await resposta.json();
 
     console.log(dados);    
+    exibirNoTopo(dados);
+});
 
-    // gerar nome
+const btnInformação = document.getElementById('btnMostrar_info');
+const caixa_informações = document.getElementById('PokeInfo')
+
+btnInformação.addEventListener('click', function(){
+
+ if( caixa_informações.style.display === 'none' || caixa_informações.style.display === '') {
+
+    caixa_informações.style.display = 'block';
+    btnInformação.textContent = 'ocultar informações';
+ }
+    else {
+        caixa_informações.style.display = 'none';
+        btnInformação.textContent = 'mostrar informações';
+    }
+
+
+})
+let limiteAtual = 30;
+let offsetAtual = 0;
+async function carregar_galeria() {
+    const linkGaleria = `https://pokeapi.co/api/v2/pokemon?limit=${limiteAtual}&offset=${offsetAtual}`;
+    let respostaGaleria = await fetch(linkGaleria);
+    let DadosGaleria = await respostaGaleria.json();
+    let listaPokemons = DadosGaleria.results;
+    const localGaleria = document.getElementById('localGaleria');
+
+   
+
+    for (let i = 0; i < listaPokemons.length; i++) {
+
+        let pokemonsGaleria = listaPokemons[i];
+        let resposta_detalhes = await fetch(pokemonsGaleria.url);
+        let dados_completos = await resposta_detalhes.json();
+
+        fazer_catão(dados_completos,localGaleria);
+       
+        
+    }
+    
+}
+
+
+function fazer_catão(dados,localNaTela){
+
+    let cartao = document.createElement('div');
+    cartao.className = 'cartaDoPokemon';
+
+    let nome = document.createElement('h3');
+    nome.textContent = dados.name;
+
+    let imagem = document.createElement('img');
+    imagem.src = dados.sprites.front_default;
+    imagem.alt = "imagem do pokemon " + nome;
+
+    
+
+    let btnVerMais = document.createElement('button')
+    btnVerMais.type = 'button';
+    btnVerMais.textContent = 'ver informações';
+    btnVerMais.addEventListener('click', function(){
+        exibirNoTopo(dados);
+    });
+
+    cartao.appendChild(nome);
+    cartao.appendChild(imagem);
+    cartao.appendChild(btnVerMais);
+    localNaTela.appendChild(cartao);
+}
+carregar_galeria();
+
+const botaoCarregarMais = document.getElementById('btnParaCarregar');
+botaoCarregarMais.addEventListener('click',function(){
+
+    offsetAtual = offsetAtual + limiteAtual;
+
+    limiteAtual =  5
+
+    carregar_galeria();
+
+
+});
+function exibirNoTopo(dados){
+     // gerar nome
     const nomePoke = document.getElementById('nomePoke');
 
     nomePoke.textContent = "";
@@ -55,6 +139,8 @@ formulario.addEventListener('submit', async function (evento ) {
 
     imagePoke_Api.src = `${dados.sprites.front_default}`;
 
+    imagePoke_Api.alt = "imagem do pokemon " + nomePoke
+
 
     imagePoke.appendChild(imagePoke_Api);
 
@@ -70,34 +156,18 @@ formulario.addEventListener('submit', async function (evento ) {
         info_create.innerHTML = `${informações_pokemon[i].chave} ${informações_pokemon[i].valor}`
 
         lista_informacoes.appendChild(info_create);
-
-
-
         
         
     }
 
+     btnInformação.style.display = 'block';
+
+     caixa_informações.style.display = 'none';
     
+     btnInformação.textContent = 'mostrar informações';
 
-    
+     const secaoTopo = document.getElementById('formularioPesquisa');
+     secaoTopo.scrollIntoView({ behavior: 'smooth' });
 
-});
+};
 
-let galeria = document.getElementById('galery');
-galeria.addEventListener('submit', async function (showM) {
-
-    showM.preventDefault();
-
-    
-})
-
-
-
-
-       
-    
-   
-    
-
-
-    carregarImgs();
